f4b9283da25dddec240813fe0383f1f822bf7efc
pizile <76762643+pizile@users.noreply.github.com>
2021-03-06 22:06:29 +0800

    Add files via upload

---
+++ Task/QQreadCOOKIE.js
@@ -0,0 +1,12 @@
+﻿//独立COOKIE文件     ck在``里面填写，多账号换行
+let QQreadurlVal = `https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywBlgVSO279l&ywguid=119202206379&usid=42_CQYD_7DWPS-bScjnuvb58KYOneFNISvw3WixAJJUx2zPhTX8a6Ajdvq_KgYFrHHHzwlHXMxuEU6_iGZ0GwbWxes8ClZGK0Z3HTqwm27a-Mo
+https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywgrCGzzT5SX&ywguid=120191083162&usid=42_16ROWFB55ha-nJ7ayNcCjlHVaQlq1PoWDfwBxZ0E9cRb3rUQOGf67o67nDWoVSf5uoslgcVdgFyvkcj40nKu2f6R81aRf40_1JJ5Bc5UcMc`
+
+
+let QQreadurlcookie = {
+
+    QQreadurlVal: QQreadurlVal,
+
+}
+
+module.exports = QQreadurlcookie
