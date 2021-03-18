990aa32ff8b8c13790648701d918105eabb13bb7
pizile <76762643+pizile@users.noreply.github.com>
2021-03-18 10:27:08 +0800

    Update iboxpayCOOKIE.js

--- Task/iboxpayCOOKIE.js
+++ Task/iboxpayCOOKIE.js
@@ -4,12 +4,14 @@
 
 let refreshtokenVal= `63d0d4bfa5d243c89d635aa3ddb01732
 7a06a9f476b14ed98b9b808b376217f0`
-
+let iboxpayvideoheaderVal= ``
+let iboxpayvideobodyVal= ``
 
 let iboxpaycookie = {
 
   refreshtokenVal: refreshtokenVal,  
-  
+    iboxpayvideoheaderVal: iboxpayvideoheaderVal,  
+      iboxpayvideobodyVal: iboxpayvideobodyVal,
 }
 
 module.exports =  iboxpaycookie
