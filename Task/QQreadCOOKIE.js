801e9d25f0459b698427d348baf6f124c3042978
pizile <76762643+pizile@users.noreply.github.com>
2021-04-01 12:30:37 +0800

    Update QQreadCOOKIE.js

--- Task/QQreadCOOKIE.js
+++ Task/QQreadCOOKIE.js
@@ -1,6 +1,6 @@
 ﻿//独立COOKIE文件     ck在``里面填写，多账号换行
 let QQreadurlVal = `https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywWhWHHUCoCy&ywguid=119202206379&usid=43_LDBIlqQrMQrmNr3vXGax8H8_1k8vxLbcuq6ixxwMuRDoJZfeGpC8cVuKuw5aflU9xxw44jCPmQy_LcobabLhIJaQvDz8XEsB-mqBdx0__5U
-https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywgrCGzzT5SX&ywguid=120191083162&usid=42_16ROWFB55ha-nJ7ayNcCjlHVaQlq1PoWDfwBxZ0E9cRb3rUQOGf67o67nDWoVSf5uoslgcVdgFyvkcj40nKu2f6R81aRf40_1JJ5Bc5UcMc`
+https://newios.reader.qq.com/v7_5_7/userinfo?usid=ywGbeEBXHvAH&uid=120191083162`
 
 
 let QQreadurlcookie = {
