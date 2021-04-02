2495aee7201b78b3705510ab917f519fa931b0f3
pizile <76762643+pizile@users.noreply.github.com>
2021-04-02 13:20:49 +0800

    Update xiaoleCOOKIE.js

--- Task/xiaoleCOOKIE.js
+++ Task/xiaoleCOOKIE.js
@@ -2,7 +2,7 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 let xiaoleurlVal= `https://minapp.xqrobot.net/user.php?mod=index&client=minapp&version=2.7&site=yyq&user_openid=ofGEO5Oph50ngCbdbFgsBkzb8DvM&user_lbs=&city=`
-let xiaoleheaderVal= `{"Cookie":"PHPSESSID=8sh2rl2ifshgm6n574a96ich54","Accept":"*/*","Connection":"keep-alive","Referer":"https://servicewechat.com/wx7d27c26991711f37/128/page-frame.html","Content-Type":"application/json","Host":"minapp.xqrobot.net","User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000236) NetType/WIFI Language/zh_CN","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-cn"}`
+let xiaoleheaderVal= `{"Cookie":"PHPSESSID=ejg9f77n1g3v6hshej1rind5g3","Accept":"*/*","Connection":"keep-alive","Referer":"https://servicewechat.com/wx7d27c26991711f37/137/page-frame.html","Content-Type":"application/json","Host":"minapp.xqrobot.net","User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000239) NetType/4G Language/zh_CN","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-cn"}`
 
 
 
