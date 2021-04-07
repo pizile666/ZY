8eed131e2dfef9accb3557b115f554090c998e48
pizile666 <82098259+pizile666@users.noreply.github.com>
2021-04-08 00:19:23 +0800

    Update xiaoleCOOKIE.js

--- Task/xiaoleCOOKIE.js
+++ Task/xiaoleCOOKIE.js
@@ -1,8 +1,10 @@
 
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let xiaoleurlVal= `https://minapp.xqrobot.net/user.php?mod=index&client=minapp&version=2.7&site=yyq&user_openid=ofGEO5Oph50ngCbdbFgsBkzb8DvM&user_lbs=&city=`
-let xiaoleheaderVal= `{"Cookie":"PHPSESSID=ejg9f77n1g3v6hshej1rind5g3","Accept":"*/*","Connection":"keep-alive","Referer":"https://servicewechat.com/wx7d27c26991711f37/137/page-frame.html","Content-Type":"application/json","Host":"minapp.xqrobot.net","User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000239) NetType/4G Language/zh_CN","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-cn"}`
+let xiaoleurlVal= `https://minapp.xqrobot.net/user.php?mod=index&client=minapp&version=2.7&site=yyq&user_openid=ofGEO5Oph50ngCbdbFgsBkzb8DvM&user_lbs=&city=
+https://minapp.xqrobot.net/user.php?mod=index&client=minapp&version=2.7&site=yyq&user_openid=&user_lbs=&city=`
+let xiaoleheaderVal= `{"Cookie":"PHPSESSID=ejg9f77n1g3v6hshej1rind5g3","Accept":"*/*","Connection":"keep-alive","Referer":"https://servicewechat.com/wx7d27c26991711f37/137/page-frame.html","Content-Type":"application/json","Host":"minapp.xqrobot.net","User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000239) NetType/4G Language/zh_CN","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-cn"}
+{"Cookie":"PHPSESSID=p3rtlgei50695a8q71hfvh74u1","Accept":"*/*","Connection":"keep-alive","Referer":"https://servicewechat.com/wx7d27c26991711f37/129/page-frame.html","Content-Type":"application/json","Host":"minapp.xqrobot.net","User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000238) NetType/WIFI Language/zh_CN","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-cn"}`
 
 
 
