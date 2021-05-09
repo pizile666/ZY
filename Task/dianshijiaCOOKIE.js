2015c371199842313d8f298738186a6be35b1c96
pizile666 <82098259+pizile666@users.noreply.github.com>
2021-05-10 07:58:27 +0800

    Update dianshijiaCOOKIE.js

--- Task/dianshijiaCOOKIE.js
+++ Task/dianshijiaCOOKIE.js
@@ -2,7 +2,7 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 let DsjurlVal= ``
 
-let DrawalVal= `http://api.gaoqingdianshi.com/api/v2/cash/withdrawal?code=randTx&rs=xPe8mvqJ4a4mSxSyszN&sign=a7c1f4c51a52018b50a248bc95ea6968`
+let DrawalVal= ``
 
 let cookiearr = {
   DsjurlVal: DsjurlVal,
